16bfbe7322623b3f827911b6065af3a7db6ee982
gaoqianshan <645992686@qq.com>
2018-09-05 02:54:53 +0800

    配置users子路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,21 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Login from '@/views/Login';
 import Home from '@/views/Home'
-
+import Users from '@/views/users/List';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {name:'login',path:'/login',component:Login},
-    {name:'Home',path:'/',component:Home}
+    {name:'Home',path:'/',component:Home,
+    children:[
+      {
+        name:'users',
+        path:'/users',
+        component:Users
+      }
+    ]
+  }
   ]
 });
