39d33525c722f46be57de94d7b2fb9056fca0d4c
gaoqianshan <645992686@qq.com>
2018-09-05 02:32:12 +0800

    用户列表页侧边栏和头部

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,65 @@
 <template>
   <el-container>
-    <el-header>Header</el-header>
+    <el-header>
+      <el-row>
+      <el-col :span="4">
+        <img src="@/assets/logo.png" alt="">
+      </el-col>
+      <el-col class="middle" :span="19">
+        <span>电商后台管理系统</span>
+      </el-col>
+      <el-col :span="1">
+        <a class="logout" href="#">退出</a>
+      </el-col>
+      </el-row>
+    </el-header>
       <el-container>
-        <el-aside width="200px">Aside</el-aside>
+        <el-aside width="200px">
+          <el-menu
+          :unique-opened="true"
+          :router="true"
+          style="height:100%"
+          efault-active="0">
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>用户管理</span>
+              </template>
+                <el-menu-item index="/users"><i class="el-icon-view"></i>用户列表</el-menu-item>
+            </el-submenu>
+            <el-submenu index="">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>权限管理</span>
+              </template>
+                <el-menu-item index="/roles"><i class="el-icon-view"></i>角色列表</el-menu-item>
+                <el-menu-item index="/rights"><i class="el-icon-view"></i>权限列表</el-menu-item>
+            </el-submenu>
+            <el-submenu index="3">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>商品管理</span>
+              </template>
+                <el-menu-item index="3-1"><i class="el-icon-view"></i>商品列表</el-menu-item>
+                <el-menu-item index="3-2"><i class="el-icon-view"></i>分类参数</el-menu-item>
+                <el-menu-item index="3-3"><i class="el-icon-view"></i>商品分类</el-menu-item>
+            </el-submenu>
+            <el-submenu index="4">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>订单管理</span>
+              </template>
+                <el-menu-item index="4-1"><i class="el-icon-view"></i>用户列表</el-menu-item>
+            </el-submenu>
+            <el-submenu index="5">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>数据统计</span>
+              </template>
+                <el-menu-item index="5-1"><i class="el-icon-view"></i>用户列表</el-menu-item>
+            </el-submenu>
+          </el-menu>
+        </el-aside>
         <el-main>Main</el-main>
     </el-container>
   </el-container>
@@ -15,6 +72,18 @@ export default {
 </script>
 
 <style>
+.logout {
+  text-decoration: none;
+  color: orange;
+}
+
+.middle {
+  text-align: center;
+  line-height: 60px;
+  font-size: 24px;
+  color: #fff;
+}
+
 .el-container {
     height: 100%;
 }
@@ -22,14 +91,15 @@ export default {
 .el-header, .el-footer {
   background-color: #B3C0D1;
   color: #333;
-  text-align: center;
+  /* text-align: center; */
   line-height: 60px;
+  padding: 0;
 }
   
 .el-aside {
   background-color: #D3DCE6;
   color: #333;
-  text-align: center;
+  /* text-align: center; */
   line-height: 200px;
 }
   
