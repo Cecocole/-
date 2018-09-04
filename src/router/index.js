dfeed0e2e5da2580aadf82b2c375ef9119ed738e
gaoqianshan <645992686@qq.com>
2018-09-05 01:00:01 +0800

    登录功能实现

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,14 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Login from '@/views/Login';
+import Home from '@/views/Home'
+
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
-    {name:'login',path:'/login',component:Login}
+    {name:'login',path:'/login',component:Login},
+    {name:'Home',path:'/',component:Home}
   ]
 });
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,15 +3,16 @@
     <el-form
         class="login-form"
         :label-position="top"
-        label-width="80px">
+        label-width="80px"
+        :model="formData">
         <h2>用户登录</h2>
         <el-form-item label="用户名">
-            <el-input></el-input>
+            <el-input v-model="formData.username"></el-input>
         </el-form-item>
         <el-form-item label="密码">
-            <el-input type="password"></el-input>
+            <el-input v-model="formData.password" type="password"></el-input>
         </el-form-item>
-            <el-button class="login-btn" type="primary">登录</el-button>
+            <el-button @click.prevent="handleLogin" class="login-btn" type="primary">登录</el-button>
         <el-form-item>
         </el-form-item>
     </el-form>
@@ -19,8 +20,43 @@
 </template>
 
 <script>
-export default {
+// 1.绑定文本框
+// 2.给按钮注册事件 发送异步请求
+// 3. 登录成功 跳转/提示/记录token
 
+export default {
+    data(){
+        return {
+            formData:{
+                username:'',
+                password:''
+            }
+        }
+    },
+    methods: {
+        // 登录
+        handleLogin() {
+            this.$http
+            .post('/login',this.formData)
+            .then((response)=>{
+                //判断登录是否成功
+                // 获取response.data中的msg和status
+                const { meta:{ msg,status } } = response.data;
+                if(status == 200) {
+                    //成功
+                    this.$message.success(msg);
+                    //记录token
+                    sessionStorage.setItem('token',response.data.data.token);
+                }else{
+                    //失败
+                    this.$message.error(msg);
+                }
+            })
+            .catch((err)=>{
+                console.log(err);
+            });
+        }
+    }
 };
 </script>
 
