d2d06b321b8f855c40a3cbc4470250b455ea6a1f
gaoqianshan <645992686@qq.com>
2018-09-08 01:08:07 +0800

    动态生成左侧菜单，让不同权限的人登录显示不同的侧边栏

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,43 +20,21 @@
           :router="true"
           style="height:100%"
           efault-active="0">
-            <el-submenu index="1">
+            <el-submenu
+            v-for="level1 in menus"
+            :key = "level1.id"
+            :index="level1.path">
               <template slot="title">
                 <i class="el-icon-location"></i>
-                <span>用户管理</span>
+                <span>{{level1.authName}}</span>
               </template>
-                <el-menu-item index="/users"><i class="el-icon-view"></i>用户列表</el-menu-item>
-            </el-submenu>
-            <el-submenu index="">
-              <template slot="title">
-                <i class="el-icon-location"></i>
-                <span>权限管理</span>
-              </template>
-                <el-menu-item index="/roles"><i class="el-icon-view"></i>角色列表</el-menu-item>
-                <el-menu-item index="/rights"><i class="el-icon-view"></i>权限列表</el-menu-item>
-            </el-submenu>
-            <el-submenu index="3">
-              <template slot="title">
-                <i class="el-icon-location"></i>
-                <span>商品管理</span>
-              </template>
-                <el-menu-item index="3-1"><i class="el-icon-view"></i>商品列表</el-menu-item>
-                <el-menu-item index="3-2"><i class="el-icon-view"></i>分类参数</el-menu-item>
-                <el-menu-item index="3-3"><i class="el-icon-view"></i>商品分类</el-menu-item>
-            </el-submenu>
-            <el-submenu index="4">
-              <template slot="title">
-                <i class="el-icon-location"></i>
-                <span>订单管理</span>
-              </template>
-                <el-menu-item index="4-1"><i class="el-icon-view"></i>用户列表</el-menu-item>
-            </el-submenu>
-            <el-submenu index="5">
-              <template slot="title">
-                <i class="el-icon-location"></i>
-                <span>数据统计</span>
-              </template>
-                <el-menu-item index="5-1"><i class="el-icon-view"></i>用户列表</el-menu-item>
+                <el-menu-item
+                v-for="level2 in level1.children"
+                :key="level2.id"
+                :index="'/' + level2.path">
+                <i class="el-icon-view"></i>
+                {{level2.authName}}
+                </el-menu-item>
             </el-submenu>
           </el-menu>
         </el-aside>
@@ -69,6 +47,16 @@
 
 <script>
 export default {
+  data() {
+    return {
+      // 获取菜单数据
+      menus:[]
+    };
+  },
+  created () {
+    // 加载菜单数据
+    this.loadMenus();
+  },
   // 判断是否登录
   beforeCreate () {
     const token = sessionStorage.getItem('token');
@@ -82,6 +70,14 @@ export default {
       sessionStorage.clear();
       this.$message.success('退出成功');
       this.$router.push('/login');
+    },
+    //加载菜单数据
+    async loadMenus() {
+      const response = await this.$http.get('menus');
+      const { meta: { msg, status } } = response.data;
+      if(status === 200) {
+        this.menus = response.data.data;
+      }
     }
   }
 };
